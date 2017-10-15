20896144e62e40f8e8761011ffa0c36887a57937
Jón Daði Pétursson <jondp16@ru.is>
2017-10-15 15:56:56 +0000

    refactored the negative number test

--- src/main/java/is/ru/Calculator.java
+++ src/main/java/is/ru/Calculator.java
@@ -2,56 +2,60 @@ package is.ru;
 
 public class Calculator{
 
-  public static int add(String text)
-  {
-    if(text.equals(""))
-    	return 0;
-    else
-    {
-    	if(has(text))
-    	{
-    		String numbers[] = text.split(",|\\n");
-		  	String negNumbers[] = new String[numbers.length];
-		  	int count = 0;
-		  	boolean hasNeg = false;
-		  	for (String number : numbers)
-			{
-				if(Integer.parseInt(number) < 0)
-				{
-					hasNeg = true;
-					negNumbers[count] = number;
-					count++;
-				}
-			}
-			if (hasNeg)
-			{
-				throw new IllegalArgumentException("negNumbers");
-			}
-
-    		return sum(numbers);
-    	}
-    	return 1;
-    }
-  }
-
-
-  private static int toInt(String number)
-  {
-  		return Integer.parseInt(number);
-  }
-
-  private static int sum(String[] numbers)
-  {
-  	int total = 0;
-	for (String number : numbers)
+	public static int add(String text)
 	{
-		total += toInt(number);
-	}
-	return total;  
-  }
-  private static boolean has(String text)
-  {
-  	return (text.contains(",") || text.contains("\n"));
-  }
+	    if(text.equals(""))
+	    	return 0;
+	    else
+	    {
+	    	if(has(text))
+	    	{
+	    		String numbers[] = text.split(",|\\n");
+			  	checkForNeg(numbers);
+    			return sum(numbers);
+    		}
+    	return 1;
+		}
+    }
+
+	private static void checkForNeg(String[] numbers)
+  	{
+  		String negNumbers[] = new String[numbers.length];
+	  	int count = 0;
+	  	boolean hasNeg = false;
+	  	for (String number : numbers)
+		{
+			if(Integer.parseInt(number) < 0)
+			{
+				hasNeg = true;
+				negNumbers[count] = number;
+				count++;
+			}
+		}
+		if (hasNeg)
+		{
+			throw new IllegalArgumentException("negNumbers");
+		}
+  	}
+
+  	private static int toInt(String number)
+  	{
+		return Integer.parseInt(number);
+  	}
+
+    private static int sum(String[] numbers)
+    {
+  		int total = 0;
+		for (String number : numbers)
+		{
+			total += toInt(number);
+		}
+		return total;  
+  	}
+  	
+  	private static boolean has(String text)
+  	{
+  		return (text.contains(",") || text.contains("\n"));
+  	}
 }
 
